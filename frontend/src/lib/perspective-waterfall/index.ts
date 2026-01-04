e7e44372186b746032250ff717e5fe3aa8af8b95
Clay Smith <smithclay@gmail.com>
2026-01-03 22:26:28 -0800

    feat(waterfall): add module index exports

---
+++ frontend/src/lib/perspective-waterfall/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Waterfall trace viewer plugin
+ */
+
+export { registerWaterfallPlugin, WaterfallElement } from './plugin';
+export { computeLayout } from './layout';
+export { parseColumnarData } from './arrow';
+export type { RawSpan, LayoutSpan, TraceLayout } from './types';
+export { LAYOUT } from './types';
+export { COLORS, getServiceColor } from './colors';
